f0539df4154f9e6e5ef38129633679274ac7eed0
shzez <156867871+shzez@users.noreply.github.com>
2024-03-06 14:29:14 +0000

    modified pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>eu te amo minha namorada dudinha, quero me casar com você</h1>
+    return <h1>eu te amo minha namorada dudinha, mal posso esperar para ter nossa casinha   </h1>
 }
 
 export default Home;
